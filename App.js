77f5314f994dd1616d821e8828c6d5df6ad6df3e
Die ZiP <diezip247@gmail.com>
2026-02-10 12:14:20 +0800

    nemelt

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ehnii udriin hicheel </Text>
+      <Text> afgsd fg g</Text>
       <StatusBar style="auto" />
     </View>
   );
